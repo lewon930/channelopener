8615d17ed418f261ad16534ef02a8fb4fc87c43f
kjw0116 <84269870+kjw0116@users.noreply.github.com>
2024-11-15 21:11:26 +0900

    fixed wrong data in js.

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ document.querySelectorAll('.item-button').forEach(button => {
   button.clickCount = 0;
 
   button.addEventListener('click', () => {
-    button.clickCount = (button.clickCount + 1) % 2;
+    button.clickCount = (button.clickCount + 1) % 3;
 
     if (button.clickCount === 1) {
       button.style.backgroundColor = 'blue';
-    }// else if (button.clickCount === 2) {
-     // button.style.backgroundColor = 'red';
-    //}
+    } else if (button.clickCount === 2) {
+      button.style.backgroundColor = 'red';
+    }
      else {
       button.style.backgroundColor = '';
     }
@@ -21,53 +21,53 @@ document.querySelectorAll('.item-button').forEach(button => {
 
 // Related items mapping
 const relatedItemsMap = {
-  "R-LU5(Magic 8ers)": ["R-LU5", "R-ST43","R-BL67",
+  "R-LU5(Magic 8ers)": ["R-ST43","R-BL67",
                         "L-LU11","L-PC7","L-BL40"
   ],
-  "L-LU5(Magic 8ers)": ["L-LU5", "L-ST43","L-BL67",
+  "L-LU5(Magic 8ers)": [ "L-ST43","L-BL67",
                         "R-LU11","R-PC7","R-BL40"],
 
-  "R-LI1(Magic 8ers)": ["R-LI1","R-TW3","R-KD10",
+  "R-LI1(Magic 8ers)": ["R-TW3","R-KD10",
                         "L-KD1","L-SP3","L-LI11"
   ],
-  "L-LI1(Magic 8ers)": ["L-LI1","L-TW3","L-KD10",
+  "L-LI1(Magic 8ers)": ["L-TW3","L-KD10",
                         "R-KD1","R-SP3","R-LI11"],
 
-  "R-BL67(Magic 8ers)": ["R-BL67","R-ST43","R-LU5",
+  "R-BL67(Magic 8ers)": ["R-ST43","R-LU5",
                           "L-LU11","L-PC7","L-BL40"
   ],
-  "L-BL67(Magic 8ers)": ["L-BL67","L-ST43","L-LU5",
+  "L-BL67(Magic 8ers)": ["L-ST43","L-LU5",
                           "R-LU11","R-PC7","R-BL40"
   ],
 
-  "R-KD10(Magic 8ers)": ["R-LI1","R-TW3","R-KD10",
+  "R-KD10(Magic 8ers)": ["R-LI1","R-TW3",
     "L-KD1","L-SP3","L-LI11"],
-  "L-KD10(Magic 8ers)": ["L-LI1","L-TW3","L-KD10",
+  "L-KD10(Magic 8ers)": ["L-LI1","L-TW3",
                         "R-KD1","R-SP3","R-LI11"],
 
-  "R-ST45(Magic 8ers)": ["R-ST45","R-GB41","R-PC3","L-PC9","L-HT7","L-ST36"],
-  "L-ST45(Magic 8ers)": ["L-ST45","L-GB41","L-PC3","R-PC9","R-HT7","R-ST36"],
+  "R-ST45(Magic 8ers)": ["R-GB41","R-PC3","L-PC9","L-HT7","L-ST36"],
+  "L-ST45(Magic 8ers)": ["L-GB41","L-PC3","R-PC9","R-HT7","R-ST36"],
 
-  "L-SP9(Magic 8ers)": ["R-TW1","R-SI3","R-SP9","L-SP1","L-LIV3","L-TW10"],
-  "R-SP9(Magic 8ers)": ["L-TW1","L-SI3","L-SP9","R-SP1","R-LIV3","R-TW10"],
+  "R-SP9(Magic 8ers)": ["R-TW1","R-SI3","L-SP1","L-LIV3","L-TW10"],
+  "L-SP9(Magic 8ers)": ["L-TW1","L-SI3","R-SP1","R-LIV3","R-TW10"],
 
-  "R-PC3(Magic 8ers)": ["R-ST45","R-GB41","R-PC3","L-PC9","L-HT7","L-ST36"],
-  "L-PC3(Magic 8ers)": ["L-ST45","L-GB41","L-PC3","R-PC9","R-HT7","R-ST36"],
+  "R-PC3(Magic 8ers)": ["R-ST45","R-GB41","L-PC9","L-HT7","L-ST36"],
+  "L-PC3(Magic 8ers)": ["L-ST45","L-GB41","R-PC9","R-HT7","R-ST36"],
 
-  "R-TW1(Magic 8ers)": ["R-TW1","R-SI3","R-SP9","L-SP1","L-LIV3","L-TW10"],
-  "L-TW1(Magic 8ers)": ["L-TW1","L-SI3","L-SP9","R-SP1","R-LIV3","R-TW10"],
+  "R-TW1(Magic 8ers)": ["R-SI3","R-SP9","L-SP1","L-LIV3","L-TW10"],
+  "L-TW1(Magic 8ers)": ["L-SI3","L-SP9","R-SP1","R-LIV3","R-TW10"],
 
-  "R-HT3(Magic 8ers)": ["R-GB44","R-BL65","R-HT3","L-HT9","L-LU9","L-GB34"],
-  "L-HT3(Magic 8ers)": ["L-GB44","L-BL65","L-HT3","R-HT9","R-LU9","R-GB34"],
+  "R-HT3(Magic 8ers)": ["R-GB44","R-BL65","L-HT9","L-LU9","L-GB34"],
+  "L-HT3(Magic 8ers)": ["L-GB44","L-BL65","R-HT9","R-LU9","R-GB34"],
 
-  "R-SI1(Magic 8ers)": ["R-SP1","R-LIV3","R-TW10","L-TW1","L-SI3","L-SP9"],
-  "L-SI1(Magic 8ers)": ["L-SP1","L-LIV3","L-TW10","R-TW1","R-SI3","R-SP9"],
+  "R-SI1(Magic 8ers)": ["R-SI3","R-LIV8","L-LIV1","L-KD3","L-SI8"],
+  "L-SI1(Magic 8ers)": ["L-SI3","L-LIV8","R-LIV1","R-KD3","R-SI8"],
 
   "R-GB44(Magic 8ers)": ["R-GB44","R-BL65","R-HT3","L-HT9","L-LU9","L-GB34"],
   "L-GB44(Magic 8ers)": ["L-GB44","L-BL65","L-HT3","R-HT9","R-LU9","R-GB34"],
 
-  "R-LIV8(Magic 8ers)": ["R-SP1","R-LIV3","R-TW10","L-TW1","L-SI3","L-SP9"],
-  "L-LIV8(Magic 8ers)": ["L-SP1","L-LIV3","L-TW10","R-TW1","R-SI3","R-SP9"],
+  "R-LIV8(Magic 8ers)": ["R-SI1","R-SI3","L-LIV1","L-KD3","L-SI8"],
+  "L-LIV8(Magic 8ers)": ["L-SI1","L-SI3","R-LIV1","R-KD3","R-SI8"],
 
 
 
